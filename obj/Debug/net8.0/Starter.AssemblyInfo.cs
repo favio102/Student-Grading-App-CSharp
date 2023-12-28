0fc52b0ced66c9543cf553359bc870e398256afd
Favio Valero <favio102@protonmail.com>
2023-12-28 07:26:44 -0800

    Calculate score inside the outer names loop

--- obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e85e84824eade37b6d4e927c03d8a22658052101")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bb9ce616cee9c580576f17e8f5d0b930bf4520e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
